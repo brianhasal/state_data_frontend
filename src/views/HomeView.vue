884f2e8949919ec0748bf2d4033e72d17c6f7d49
Brian Hasal <brian.hasal@gmail.com>
2022-09-03 06:08:41 -0700

    Pulling Info from Abbr and displaying

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        state_data_array: [],
+        abbr_array: []
+      };
+    },
+    created: function () {
+      this.indexStateData();
+    },
+    methods: {
+      indexStateData: function () {
+        axios.get("/states.json").then((response) => {
+          console.log(response.data);
+          this.abbr_array = response.data;
+        })
+      }
+    },
+  };
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>States by Abbreviation</h1>
+    <p>{{this.abbr_array}}</p>
+
+    <div v-for="state in abbr_array" v-bind:key="state.name">
+      <h2>{{state["state"]}}</h2>
+      <h3>{{state["abbrev"]}}</h3>
+      <h3>{{state["code"]}}</h3>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
